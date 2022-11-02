4792f2e3732417cf68bdb388169064b01489daab
Karina-Moveo <karinab@moveo.co.il>
2022-11-02 15:32:21 +0200

    Fix to match typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Homepage from './components/pages/homepage/Homepage'
+// import { useSelector, useDispatch } from 'react-redux'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
