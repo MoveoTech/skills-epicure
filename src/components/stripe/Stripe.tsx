b6ba16e07485503d7f48f45892972e735dd2bbff
Karina-Moveo <karinab@moveo.co.il>
2022-11-09 15:18:06 +0200

    Added stripe design

--- src/components/stripe/Stripe.tsx
+++ src/components/stripe/Stripe.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { IRestaurant } from '../../interfaces/IRestaurant.interface'
+import { fetchPopularRestaurants } from '../../services/dataService.service'
 import './Stripe.css'
 
 interface StripeProps {
@@ -7,13 +9,23 @@ interface StripeProps {
 }
 
 const Stripe = (props : StripeProps) => {
+    const [restaurants, setRestaurants] = useState<Array<IRestaurant>>()
+
+    useEffect(() => {
+        (async () => {
+        const data = await fetchPopularRestaurants()
+        setRestaurants(data as Array<IRestaurant>)
+        })()
+    }, [])
+
     return (
         <div className='stripe-container'>
             <h2 className='title'>{props.title}</h2>
+            <div className='cards-container'>
+            </div>
             { props.all ? 
             <div className='all-container'>
                 <h2 className='all-text'>All Restaurants</h2>
-                {/* <img src={arrows}/> */} 
             </div> 
             : ""}
         </div>
