a8233f3b52a4a99f5f0d6e3fc94efdfe60e9062e
Karina-Moveo <karinab@moveo.co.il>
2022-11-13 16:26:31 +0200

    Added about section

--- src/services/dataService.service.ts
+++ src/services/dataService.service.ts
@@ -4,6 +4,14 @@ export const fetchHeroImage = async () => {
     return (await Promise.resolve(db)).hero
 }
 
+export const fetchLogo = async () => {
+    return (await Promise.resolve(db)).logo
+}
+
 export const fetchIcons = async () => {
     return (await Promise.resolve(db)).icons
+}
+
+export const fetchAbout = async () => {
+    return (await Promise.resolve(db)).about
 }
