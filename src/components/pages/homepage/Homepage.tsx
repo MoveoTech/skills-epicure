4210cb35d9394d0dd1fdf799a45289e554d6ff5f
Karina-Moveo <karinab@moveo.co.il>
2022-11-17 16:51:08 +0200

    Added footer to homepage

--- src/components/pages/homepage/Homepage.tsx
+++ src/components/pages/homepage/Homepage.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Banner from '../../banner/Banner';
+import Footer from '../../footer/Footer';
 import Header from "../../header/Header";
 import Stripe from '../../stripe/Stripe';
 
 function Homepage() {
   return (
-      <div className="container">
+      <div className="homepage-container">
         <Header/>
         <Banner/>
         <Stripe title="popular restaurant in epicure:" all={true} type="restaurants"/>
         <Stripe title="signature dish of:" all={false} type="dishes"/>
+        <Footer/>
       </div>
   );
 }
