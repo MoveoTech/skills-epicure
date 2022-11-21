dc94a5738bd279e9a62dc64e16ddbcf87e9ba702
Karina-Moveo <karinab@moveo.co.il>
2022-11-21 13:26:16 +0200

    Added chef image and description to chef stripe

--- src/components/chef/Chef.tsx
+++ src/components/chef/Chef.tsx
@@ -1,11 +1,31 @@
 import './Chef.css'
 import { useState, useEffect } from 'react'
+import { IChef } from '../../interfaces/IChef.interface'
+import { fetchChefOfWeek } from '../../services/dataService.service'
 
 
 function Chef() {
+    const [chef, setChef] = useState<IChef>()
+
+    useEffect(() => {
+      (async () => {
+        const data = await fetchChefOfWeek()
+        setChef(data as IChef)
+      })()
+    }, [])
 
   return (
-      <div>
+      <div className='chef-container'>
+        <h2 className='title'>Chef of the week:</h2>
+        
+        {chef ? 
+        <div className='chef-desc'>
+            <div className='chef-image'>
+                <img src={require(`../../resources/images/${chef.image}`)} alt={chef?.last_name}/>
+                <h3 className='chef-name'>{chef.first_name + " " + chef.last_name}</h3>
+            </div>
+            <h5>{chef.desc}</h5>
+        </div> : "" }
       </div>
   )
 }
