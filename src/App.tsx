d430b03eb61eab62f0217a8838f79a698207ce3b
Karina-Moveo <karinab@moveo.co.il>
2022-11-22 16:16:45 +0200

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 import Homepage from './components/pages/homepage/Homepage'
-// import { useSelector, useDispatch } from 'react-redux'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Restaurants from './components/pages/restaurants-page/Restaurants';
 
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Homepage/>} />
+          <Route path='/restaurants' element={<Restaurants/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
