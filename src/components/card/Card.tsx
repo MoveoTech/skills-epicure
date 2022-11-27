c9d4e245e41aec57724edab3fc1dd98da4eb609a
Karina-Moveo <karinab@moveo.co.il>
2022-11-27 15:29:47 +0200

    Added nav to restaurant page and general design

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom'
 import { IDish } from '../../interfaces/IDish.interface'
 import { IRestaurant } from '../../interfaces/IRestaurant.interface'
 import Rating from '../rating/Rating'
@@ -11,6 +12,8 @@ interface CardProps {
 function Card (props : CardProps) {
     const totalStars = 5
     let card = createCardWithType(props.type, props.data)
+    const navigate = useNavigate()
+
 
     function createCardWithType(type: string, data: any) {
       const curr = "currency.svg"
@@ -19,7 +22,7 @@ function Card (props : CardProps) {
         case 'restaurants':
         case'restaurants-page':
           const restaurant = data as IRestaurant
-          description = <div className='card-container'>
+          description = <div className='card-container' onClick={() => navigate(`/restaurant-page/${restaurant.name}`, {state:{data: restaurant}})}>
           <img src={require(`../../resources/images/${data.image}`)} className="card-image" alt={data.image}/> 
             <div className='desc-container'>
               <h3>{restaurant.name}</h3>
@@ -27,7 +30,7 @@ function Card (props : CardProps) {
               <Rating total={totalStars} rating = {restaurant.rating}/>
             </div>
           </div>
-          break
+        break
         case 'dishes':
           const dish = data as IDish
           description = <div className='card-container'>
@@ -46,10 +49,29 @@ function Card (props : CardProps) {
               </div>}
             </div>
           </div>
-          break
+        break
+        case 'restaurant-dishes':
+          const restaurantDish = data as IDish
+          description = <div className='card-container'>
+          <img src={require(`../../resources/images/${data.image}`)} className="card-image restaurant-dish-card-image" alt={data.image}/> 
+            <div className='desc-container restaurant-dish-desc'>
+              <h3 className='restaurant-dish-text'>{restaurantDish.name}</h3>
+              <h5 className='restaurant-dish-desc-text'>{restaurantDish.desc}</h5>
+              { restaurantDish.special ? (<img src={require(`../../resources/icons/${restaurantDish.special}.svg`)} className="specials-icon" key={restaurantDish.name} alt={restaurantDish.name}/>) : 
+              <div className='cost-container'>
+                  <div className='line line-restaurant-dish'/>
+                  <h5 className='h-cost restaurant-dish-cost'>
+                    <img src={require(`../../resources/icons/${curr}`)} className="cost-icon" alt="nis"/>
+                    {restaurantDish.cost}
+                  </h5>
+                  <div className='line line-restaurant-dish'/>
+              </div>}
+            </div>
+          </div>
+        break
         case 'weekly':
           const chefRestaurant = data as IRestaurant
-          description = <div className='card-container'>
+          description = <div className='card-container' onClick={() => navigate(`/restaurant-page/${chefRestaurant.name}`, {state:{data: chefRestaurant}})}>
           <img src={require(`../../resources/images/${data.image}`)} className="card-weekly" alt={data.image}/> 
             <div className='desc-container card-weekly-desc'>
               <h2 className='chef-restaurant-title'>{chefRestaurant.name}</h2>
@@ -57,7 +79,7 @@ function Card (props : CardProps) {
           </div>
         break
         default:
-          break
+        break
       }
     
       return description
